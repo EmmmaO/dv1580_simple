45d9fdf5cad41fb41997fdd35c6619cb7728cc92
Emma <mail>
2024-09-18 21:49:05 +0200

    added isdigit check for add and sub

--- add.c
+++ add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[]){
 
@@ -9,12 +10,16 @@ if (argc < 3) {
         printf("Not enough arguments");
         return 1;
     }
-
+if (isdigit(*argv[1]) && isdigit(*argv[2]))
+{
     int num1 = atoi(argv[1]);
     int num2 = atoi(argv[2]);
     int sum = num1 + num2;
 
     printf("%d", sum);
+}
+else
+    printf("Arguments should only be integers!");
     
     return 0;
 }
--- sub.c
+++ sub.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[]){
 
@@ -9,13 +10,17 @@ int main(int argc, char *argv[]){
     return 1;
   }
     
-
+  if(isdigit(*argv[1] && isdigit(*argv[2])))
+  {
     int num1 = atoi(argv[1]);
     int num2 = atoi(argv[2]);
 
     int diff = num1 - num2;
 
     printf("%d", diff);
+  }
+  else
+  printf("NOT INTEGERS!");
 
   return 0;
 }
